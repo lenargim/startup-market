a703c38008168992dace5a12300452b26c48d6f5
Lenar <linz14@mail.ru>
2022-09-15 22:46:41 +0300

    Main page

--- app/js/swiper.js
+++ app/js/swiper.js
@@ -1,4 +1,16 @@
-const swiper = new Swiper('.project__slider', {
+const projectSwiper = new Swiper('.project__slider', {
+  slidesPerView: 'auto',
+  speed: 500,
+  spaceBetween: 40,
+  allowTouchMove: false,
+  watchSlidesProgress: true,
+  navigation: {
+    prevEl: '.navigation__prev',
+    nextEl: '.navigation__next',
+  },
+});
+
+const mainSwiper = new Swiper('.main-page__slider', {
   slidesPerView: 'auto',
   speed: 500,
   spaceBetween: 40,
